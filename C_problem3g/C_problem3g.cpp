57f21a35309b3036c24381f270f373b6a5857635
Muthupriya <muthupriyag114@gmail.com>
2026-06-05 23:21:27 +0530

    Assignment_3

---
+++ C_problem3g/C_problem3g.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int x,o,t;
+	printf("Enter the 2 digit number:");
+	scanf("%d",&x);
+	t=x/10;
+	o=x%10;
+	if(t==o){
+		printf("Success");
+	}
+	else{
+		printf("Failure");
+	}
+}
