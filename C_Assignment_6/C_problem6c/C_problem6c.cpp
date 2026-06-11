6235b5a3f271408cdb102268d445bf1e106f1135
Muthupriya <muthupriyag114@gmail.com>
2026-06-11 23:03:04 +0530

    Assignment6

---
+++ C_Assignment_6/C_problem6c/C_problem6c.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int x=1,a=0;
+	while(x<6){
+		a=a+x;
+		x++;
+	
+	}
+	printf("%d\n",a);
+}
---
+++ C_Assignment_6/C_problem6e/C_problem6e.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int x=1,a;
+	while(x<10){
+		a=x%2;
+		if(a==1){
+			printf("%d\n",x);
+		}
+		x++;
+	
+	}
+
+}
