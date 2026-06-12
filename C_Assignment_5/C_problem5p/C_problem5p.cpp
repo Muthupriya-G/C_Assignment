cfaaefb48b1f969f82491d9b70cfd527cb0dec17
Muthupriya <muthupriyag114@gmail.com>
2026-06-13 00:06:07 +0530

    Assignment 5

--- C_Assignment_5/C_problem5p/C_problem5p.cpp
+++ C_Assignment_5/C_problem5p/C_problem5p.cpp
@@ -14,32 +14,32 @@ int main()
 }
 	if(b==1){
 		printf("Not Prime");
-		for(j=0;j<n;j++){
+		for(j=0;j<i;j++){
 			z=x%10;
 			d=x/10;
 			e=e+a;
 			x=d;
 			}
 			if(e==14){
-				prinf("The number is Not prime but sum 14:%d",f);
+				printf("The number is Not prime but sum 14:%d",f);
 			}
 			else{
-				prinf("The number is Not prime but sum is not 14:%d",f);
+				printf("The number is Not prime but sum is not 14:%d",f);
 			}
 	}		
 	else{
 		printf("Prime");
-		for(j=0;j<n;j++){
+		for(j=0;j<i;j++){
 			z=x%10;
 			d=x/10;
 			e=e+a;
 			x=d;
 			}
 			if(e==14){
-				prinf("The number is Not prime but sum 14:%d",f);
+				printf("The number is Not prime but sum 14:%d",f);
 			}
 			else{
-				prinf("The number is Not prime but sum is not 14:%d",f);
+				printf("The number is Not prime but sum is not 14:%d",f);
 			}
 	}
 }
