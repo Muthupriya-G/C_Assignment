f7cc90fed29dfd6dccc5094facbf89ab6e381c18
Muthupriya <muthupriyag114@gmail.com>
2026-07-06 23:01:14 +0530

    Assignment8

---
+++ C_Assignment_8/8q/8q.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int disp_interchange_first_last_digit(int);
+int main(){
+	int x,y;
+	printf("Enter the number:");
+	scanf("%d",&x);
+	y=disp_interchange_first_last_digit(x);
+	printf("%d",y);
+}
+int disp_interchange_first_last_digit(int x){
+	int y,z,f,l,p=1,m,r,n=0,i,temp,j,a,k,b=1,c,d;
+	temp=x;
+	while(x!=0){
+		x=x/10;
+		n++;
+	}
+	for(i=1;i<n;i++){
+		p = p * 10;
+	}
+	a=i-1;
+	for(k=1;k<a;k++){
+		b = b*10;
+	}
+	l=temp%10;
+    f=temp/p;
+    z=temp/10;
+    m=z-(f*b);
+	r=l*p + m*10 +f;
+	return r;
+}
